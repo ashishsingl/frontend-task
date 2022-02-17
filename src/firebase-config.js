c3e6c5ef3318d03116421a18e7d2d2ac0a29a55f
Ashish Singla <82920890+ashishsingl@users.noreply.github.com>
2022-02-17 20:19:49 +0530

    final commits

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8U-JKbj8rwD44vtKmTxSpVH2yz4UWUdc",
-  authDomain: "frontend-assignment-8920c.firebaseapp.com",
-  projectId: "frontend-assignment-8920c",
-  storageBucket: "frontend-assignment-8920c.appspot.com",
-  messagingSenderId: "801461859840",
-  appId: "1:801461859840:web:36568299b5aad6720ccae5",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSEGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
